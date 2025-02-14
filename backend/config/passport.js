bfaeb736b319ff0eb169d0e68d772b897bcaecaf
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-02-14 13:53:19 +0800

    Setting Up Google OAuth

---
+++ backend/config/passport.js
@@ -0,0 +1,19 @@
+//Manages authentication & session storage
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user");
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+}, async (accessToken, refreshToken, profile, done) => {
+  let user = await User.findByEmail(profile.emails[0].value);
+  if (!user) {
+    user = await User.createUser(profile.displayName, profile.emails[0].value, null);
+  }
+  done(null, user);
+}));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => done(null, id));
