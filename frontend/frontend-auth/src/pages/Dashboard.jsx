d8b920ec37332b749f549d162cc1d779de96d363
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-02-18 14:50:00 +0800

    importing css for all

--- frontend/frontend-auth/src/pages/Dashboard.jsx
+++ frontend/frontend-auth/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
